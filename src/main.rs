83c24d64a9afe2ca8c4677f45e85a2be76016040
xorrior <christopher.ross@zoom.us>
2023-05-07 08:34:25 -0400

    second commit

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,103 @@
-#![feature(asm)]
+#![windows_subsystem = "windows"]
 
-use std::arch::asm;
 use std::mem;
-use std::ffi::CString;
+use std::ptr;
+use winapi::ctypes::c_void;
+use winapi::shared::ntdef::NTSTATUS;
+use winapi::shared::ntstatus::STATUS_SUCCESS;
+use winapi::um::memoryapi::VirtualProtect;
+use winapi::um::libloaderapi::{GetModuleHandleA, GetProcAddress};
+use winapi::um::processthreadsapi::GetCurrentProcess;
+use winapi::um::winnt::{
+    MEM_COMMIT, MEM_RELEASE, PAGE_EXECUTE_READWRITE, PAGE_EXECUTE_WRITECOPY, PAGE_READWRITE,
+};
+
+macro_rules! syscall {
+    ($syscall_number:expr) => {{
+        let result: NTSTATUS;
+        unsafe {
+            asm!(
+                "mov eax, {syscall_number}",
+                "mov edx, 0x7ffe0300", // Arbitrary non-zero value
+                "syscall",
+                "mov dword ptr [rsp], eax",
+                out("eax") result,
+            );
+        }
+        result
+    }};
+    ($syscall_number:expr, $($arg:expr),*) => {{
+        let result: NTSTATUS;
+        unsafe {
+            asm!(
+                "mov eax, {syscall_number}",
+                $(
+                    "mov {}, {}",
+                    lateout("rcx") $arg,
+                    in(reg) $arg,
+                )*
+                "syscall",
+                "mov dword ptr [rsp], eax",
+                out("eax") result,
+            );
+        }
+        result
+    }};
+}
 
 fn main() {
     let array_size = 10;
-    let syscall_mmap: usize = 9;
-    let syscall_mprotect: usize = 10;
-    let syscall_munmap: usize = 11;
+    let buffer: Vec<u8> = vec![0; array_size];
 
-    let mut buffer: Vec<u8> = vec![0; array_size];
+    // Allocate memory
+    let mem_ptr = unsafe {
+        syscall!(
+            0x18, // NtAllocateVirtualMemory syscall number
+            GetCurrentProcess(),
+            &mut mem_ptr as *mut *mut c_void,
+            0,
+            &mut array_size as *mut usize,
+            MEM_COMMIT,
+            PAGE_READWRITE
+        );
+        mem_ptr
+    };
 
-    // Allocate memory using mmap syscall
-    let mmap_ptr: *mut u8 = mmap(
-        0 as *mut u8,
-        array_size,
-        libc::PROT_READ | libc::PROT_WRITE | libc::PROT_EXEC,
-        libc::MAP_PRIVATE | libc::MAP_ANONYMOUS,
-        -1,
-        0,
-    );
-
-    if mmap_ptr == libc::MAP_FAILED {
+    if mem_ptr.is_null() {
         panic!("Memory allocation failed");
     }
 
     // Write the byte array to the allocated memory
     unsafe {
-        libc::memcpy(mmap_ptr as *mut libc::c_void, buffer.as_ptr() as *const libc::c_void, array_size);
+        ptr::copy_nonoverlapping(buffer.as_ptr(), mem_ptr as *mut u8, array_size);
     }
 
-    // Cast the address to a function pointer and execute it
-    let func_ptr: extern "C" fn() = unsafe { mem::transmute(mmap_ptr) };
-    func_ptr();
-
-    // Free the allocated memory using munmap syscall
-    munmap(mmap_ptr as *mut libc::c_void, array_size);
-}
-
-fn mmap(
-    addr: *mut u8,
-    length: usize,
-    prot: libc::c_int,
-    flags: libc::c_int,
-    fd: libc::c_int,
-    offset: libc::off_t,
-) -> *mut u8 {
-    let mmap_ptr: *mut u8;
-
+    // Change memory protection to allow execution
+    let old_protect: usize = 0;
     unsafe {
-        asm!(
-        "syscall",
-        in("rax") 9,
-        in("rdi") addr,
-        in("rsi") length,
-        in("rdx") prot,
-        in("r10") flags,
-        in("r8") fd,
-        in("r9") offset,
-        lateout("rax") mmap_ptr,
+        syscall!(
+            0x19, // NtProtectVirtualMemory syscall number
+            GetCurrentProcess(),
+            &mut mem_ptr as *mut *mut c_void,
+            &mut array_size as *mut usize,
+            PAGE_EXECUTE_READWRITE,
+            &mut old_protect as *mut usize,
         );
     }
 
-    mmap_ptr
-}
+    // Cast the address to a function pointer and execute it
+    let func_ptr = mem_ptr as *const ();
+    let func: extern "C" fn() = unsafe { std::mem::transmute(func_ptr) };
+    func();
 
-fn munmap(addr: *mut libc::c_void, length: usize) {
+    // Free the allocated memory
     unsafe {
-        asm!(
-        "syscall",
-        in("rax") 11,
-        in("rdi") addr,
-        in("rsi") length,
+        syscall!(
+            0x1A, // NtFreeVirtualMemory syscall number
+            GetCurrentProcess(),
+            &mut mem_ptr as *mut *mut c_void,
+            &mut array_size as *mut usize,
+            MEM_RELEASE
         );
     }
 }
